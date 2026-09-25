35b64f05afa83d77b57421b601ec63426c219a7b
fredrickchigwada-cmyk <fredrickchisi855@gmail.com>
2026-09-25 20:48:38 +0200

    Add reliable GitHub APK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.thefxxau.bot"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.thefxxau.bot"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "0.1.0"
     }
